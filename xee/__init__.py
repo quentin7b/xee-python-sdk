8f1dbaf90cc8fb9ae3d60f018b500742b4a03127
tikismoke <webmaster@poudot.fr>
2020-04-27 22:56:17 +0200

    add scope to init

--- xee/__init__.py
+++ xee/__init__.py
@@ -6,3 +6,16 @@
 """
 
 from .sdk import Xee
+
+from enum import Enum
+
+class AuthScope(Enum):
+    """Authorization scopes."""
+    VEHICLES_MANAGEMENT = "vehicles.management"
+    VEHICLES_READ = "vehicles.read"
+    VEHICLES_READ_SIGNALS = "vehicles.signals.read"
+    VEHICLES_READ_LOCATIONS = "vehicles.locations.read"
+    VEHICLES_READ_EVENTS = "vehicles.events.read"
+    VEHICLES_READ_ACCELEROMETERS = "vehicles.accelerometers.read"
+    VEHICLES_READ_DEVICE_DATA = "vehicles.devices-data.read"
+    ACCOUNT_READ = "account.read"
